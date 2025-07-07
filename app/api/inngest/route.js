dd1a2dd384b637bc03512f40bd479a432668eb64
dishagargg <129649341+dishagargg@users.noreply.github.com>
2025-07-07 21:35:58 +0530

    Inngest

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,9 +1,16 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "@/lib/inngest/client";
-import { generateIndustryInsights } from "@/lib/inngest/function";
+// import { generateIndustryInsights } from "@/lib/inngest/function";
+import { hellWord } from "@/lib/inngest/function";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [generateIndustryInsights],
+  // functions: [generateIndustryInsights],
+  functions: [
+    helloWord,
+  ],
 });
+
+
+
